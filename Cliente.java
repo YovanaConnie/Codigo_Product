75364e0167612f7390d852ca0a62546361ecd385
YovanaConnie <yovanaroca@gmail.com>
2025-10-24 17:38:57 -0500

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author Seba_
+ */
+public class Cliente {
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    private String nombre;
+    private String telefono;
+    private String email;
+    private String dni;
+    private String direccion;
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Cliente(String nombre, String direccion, String telefono, String email, String dni) {
+    this.nombre = nombre;
+    this.direccion = direccion;
+    this.telefono = telefono;
+    this.email = email;
+    this.dni = dni;
+}
+}
